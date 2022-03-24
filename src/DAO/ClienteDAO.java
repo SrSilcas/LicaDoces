0e9c3d340a1ec42063ef1827d0456cf3f83eb0b0
Lucas Silva <luccassilva0369@gmail.com>
2022-03-23 22:00:05 -0300

    LicaDoces4

--- src/DAO/ClienteDAO.java
+++ src/DAO/ClienteDAO.java
@@ -4,10 +4,11 @@ import utils.JDBCultil;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ClienteDAO {
-    public void salvarProduto(Cliente cliente) throws SQLException {
+    public Cliente salvarProduto(Cliente cliente) throws SQLException {
         String sql = "INSERT INTO cliente" +
                 "(nome, numero, bairro_de_entrega_1, bairro_de_entrega_2, bairro_de_entrega_3, bairro_de_entrega_4," +
                 " bairro_de_entrega_5, data_aniverrsario, data_aniversario_parente, data_aniversario_parente2)" +
@@ -36,5 +37,46 @@ public class ClienteDAO {
         }catch (SQLException e){
             System.out.println("Falha ao tentar salvar o cliente");
     }
+
+        public Cliente pesquisar(Long, id){
+
+            Connection conexao = null;
+
+            String sql1 = "SELECT * FROM produto WHERE id=?";
+
+            PreparedStatement ps2 = conexao.prepareStatement(sql1);
+
+            ps2.setLong(1,1l);
+            ResultSet resultSet = ps2.executeQuery();
+
+            try{
+            if (resultSet.next()){
+                Cliente clientePesquisado = new Cliente();
+
+                clientePesquisado.setId(resultSet.getLong("id"));
+                clientePesquisado.setNome(resultSet.getString("nome"));
+                clientePesquisado.setNumero(resultSet.getInt("numero"));
+                clientePesquisado.setBairroEntrega1(resultSet.getString("bairro_de_entrega_1"));
+                clientePesquisado.setBairroEntrega2(resultSet.getString("bairro_de_entrega_2"));
+                clientePesquisado.setBairroEntrega3(resultSet.getString("bairro_de_entrega_3"));
+                clientePesquisado.setBairroEntrega4(resultSet.getString("bairro_de_entrega_4"));
+                clientePesquisado.setBairroEntrega5(resultSet.getString("bairro_de_entrega_5"));
+                clientePesquisado.setDataAniversario(resultSet.getString("data_aniverrsario"));
+                clientePesquisado.setDataAniversarioParente(resultSet.getString("data_aniversario_parente"));
+                clientePesquisado.setDataAniversarioParente2(resultSet.getString("data_aniversario_parente2"));
+
+                ps2.execute();
+
+                conexao.close();
+                return clientePesquisado;
+            }
+
+            }catch (SQLException e){
+
+                System.out.println("Falha ao pesquisar cliente");
+
+            }
+        }
     }
+
 }
